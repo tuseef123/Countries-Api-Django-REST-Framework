7bcbb6db205497484cc22aa934487e3a5d9455ac
tuseef123 <tossefashraf.292791@gmail.com>
2021-01-04 01:39:24 +0500

    🐛 fix the bug

--- countries/serializers.py
+++ countries/serializers.py
@@ -5,5 +5,6 @@ from countries.models import Countries
 class CountriesSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Countries,
+        model = Countries
+        #after countries remove the , to make it working perfectly
         fields = ('id','name','capital')
--- countries/urls.py
+++ countries/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/countries$',views.countries_details),
-    url(r'^api/countries/(?P<pk>[0-9]+)$',views.countries_details)
+    url(r'^api/countries$', views.countries_list),
+    url(r'^api/countries/(?P<pk>[0-9]+)$', views.countries_detail)
 ]
